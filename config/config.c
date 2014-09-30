c5ed8a6e5da8645e2b7aff6b2cce53ca48e32df7
= <=>
2014-09-30 10:43:51 +0800

    update and fix some bugs

--- config/config.c
+++ config/config.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "config.h"
-
 
 
 config_t conf = {
@@ -15,9 +15,10 @@ config_t conf = {
 
 static
 char * trim_left(char *str) {
+	char *p;
 	if(str == NULL)
 		return NULL;
-	char *p = str;
+	p = str;
 	while(*p == ' ' && *p != '\0') {
 		++p;
 	}
@@ -26,10 +27,11 @@ char * trim_left(char *str) {
 
 static
 char * trim_right(char *str) {
+	int len;
 	if(str == NULL)
 		return NULL;
 
-	int len = strlen(str);
+	len = strlen(str);
 	while(len) {
 		if(*(str+len-1) == ' ') {
 			*(str+len-1) = '\0';
@@ -43,8 +45,10 @@ char * trim_right(char *str) {
 
 static
 int get_kv(char *line, char **key, char **value) {
+	char *p;
+	char *t;
 	trim_left(line);
-	char *p = strchr(line, '=');
+	p = strchr(line, '=');
 	if(p == NULL) {
 		fprintf(stderr, "BAD FORMAT:%s\n", line);
 		return -1;
@@ -56,7 +60,7 @@ int get_kv(char *line, char **key, char **value) {
 	
 
 	//remove the '\n' at the end of the line
-	char *t = strrchr(*value, '\n');
+	t = strrchr(*value, '\n');
 	if(t != NULL)
 		*t = '\0';
 	t = strrchr(*value, '\r');
@@ -69,23 +73,25 @@ int get_kv(char *line, char **key, char **value) {
 
 
 int read_config(config_t *conf) {
+	char config_line[512];
 	FILE *fp = fopen(CONFIG_FILE_PATH, "r");
 	if(fp == NULL) {
-		fprintf(stderr, "Can not open config file\n");
+		fprintf(stderr, "Can not open config file:%s\n", strerror(errno));
 		return -1;
 	}
-
-	char config_line[512];
+	
 	while(!feof(fp)) {
+		int ret;
+		char *key = NULL;
+		char *value = NULL;
 		memset(config_line, 0, sizeof(config_line));
 		fgets(config_line, 512, fp);
 		//printf("+++%s", config_line);
 		trim_left(config_line);
 		if(config_line[0] == '#' || config_line[0] == '\n'|| config_line[0] == '\r' || strcmp(config_line, "") == 0)
 			continue;
-		char *key = NULL;
-		char *value = NULL;
-		int ret = get_kv(config_line, &key, &value);
+		
+		ret = get_kv(config_line, &key, &value);
 		// if(ret != -1)
 		// 	printf("key:-%s-\nvalue:-%s-\n", key, value);
 		if(ret != -1) {
@@ -99,16 +105,6 @@ int read_config(config_t *conf) {
 		}
 		
 	}
-
+	fclose(fp);
 	return 0;
 }
-
-// int main()
-// {
-// 	read_config(&conf);
-// 	printf("input:%s--\n", conf.input_path);
-// 	printf("output:%s--\n", conf.output_path);
-// 	printf("show:%d--\n", conf.show_mid_steps);
-
-// 	return 0;
-// }
--- config/demo.c
+++ config/demo.c
@@ -3,7 +3,11 @@
 
 int main()
 {
-	read_config(&conf);
+	int ret;
+	ret = read_config(&conf);
+	if(ret == -1) {
+		return -1;
+	}
 	printf("input:%s\n", conf.input_path);
 	printf("output:%s\n", conf.output_path);
 	printf("show:%d\n", conf.show_mid_steps);
